65669038f1b624b02223c0d5574d2e6919b99afc
Nabil <ncherifi2@gmail.com>
2020-08-09 16:04:03 +0200

    this is the second commit

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,19 +81,10 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Wellcome Nabil To yor site: your site is ready !!!!
+                    <H1>Thanks you !! ;)</H1>
                 </div>
 
-                <div class="links">
-                    <a href="https://laravel.com/docs">Docs</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://blog.laravel.com">Blog</a>
-                    <a href="https://nova.laravel.com">Nova</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://vapor.laravel.com">Vapor</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
-                </div>
             </div>
         </div>
     </body>
